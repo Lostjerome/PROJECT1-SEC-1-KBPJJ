fb037f88aa5b8c24ec6ca472e631f80b5e427526
jerome <naronkrach@gmail.com>
2023-02-18 21:29:10 +0700

    :fire: Removed extra code

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,17 @@ import { ref, computed } from "vue";
 import moon from "./components/icons/moon.vue";
 import sun from "./components/icons/sun.vue";
 
-const showQuestion = ref(false);
-const showResultPopup = ref(false);
 const showHowtToPlay = ref(false);
-const showAnswer = ref(false);
 
 const question = ref([]);
 const categories = ref();
-const score = ref(0);
-const currentQuestion = ref(1);
-const process = ref(10);
 
-const warnMode = ref(false);
 const nightMode = ref(false);
 
 const category = ref("");
 const difficulty = ref("");
+
+const isPlaying = ref(false);
 
 const difficulties = [
   {
@@ -41,18 +36,6 @@ const difficulties = [
   },
 ];
 
-const choices = computed(() => {
-  let choice = [];
-  if (question.value[currentQuestion.value - 1]) {
-    choice = [
-      ...question.value[currentQuestion.value - 1].incorrectAnswers,
-      question.value[currentQuestion.value - 1].correctAnswer,
-    ];
-    shuffle(choice);
-  }
-  return choice;
-});
-
 fetch("https://the-trivia-api.com/api/categories")
   .then((response) => response.json())
   .then((data) => (categories.value = data));
@@ -68,58 +51,9 @@ function getQuestion(category, difficulty) {
   return randomQuestion(question.value);
 }
 
-//set for start quiz again
-const setNewQuiz = () => {
-  showResultPopup.value = false;
-  score.value = 0;
-  category.value = "";
-  difficulty.value = "";
-  currentQuestion.value = 1;
-  process.value = 10;
-};
-
-// check user answer and go to next question
-const choiceOnClick = (answer, correctAnswer) => {
-  showAnswer.value = true;
-  if (answer === correctAnswer) {
-    score.value += 1;
-  }
-  setTimeout(() => {
-    currentQuestion.value += 1;
-    process.value += 10;
-    showAnswer.value = false;
-  }, 500);
-};
-
-// show result when user answer all question
-const showResult = () => {
-  if (currentQuestion.value === 10) {
-    setTimeout(() => {
-      showQuestion.value = false;
-      showResultPopup.value = true;
-    }, 500);
-  }
-};
-
-// check that user select category and difficulty
-const checkOption = () => {
-  if (category.value === "" || difficulty.value === "") {
-    warnMode.value = true;
-    showQuestion.value = false;
-  } else {
-    warnMode.value = false;
-    showQuestion.value = true;
-    getQuestion(category.value, difficulty.value);
-  }
-};
-
 function randomQuestion(questions) {
   const random = Math.floor(Math.random() * 10);
   return questions[random];
-}
-
-function shuffle(array) {
-  return array?.sort(() => Math.random() - 0.5);
 }
 
 const darkModeSet = (item) => {
@@ -140,138 +74,159 @@ const darkModeSet = (item) => {
 </script>
 
 <template>
-  <div :class="nightMode ? darkModeSet('bg') : ''" class="h-full">
-    <!-- night mode btn -->
-    <div class="fixed top-12 right-1 md:right-8">
-      <label
-        class="swap swap-rotate neutral justify-center mr-6 w-10 h-10 rounded-full shadow-lg"
-        :class="nightMode ? darkModeSet('block') : 'bg-slate-200'"
-      >
-        <input type="checkbox" v-model="nightMode" />
-        <moon class="swap-on w-5 h-5" />
-        <sun class="swap-off w-5 h-5" />
-      </label>
-    </div>
-
-    <!-- header -->
-    <div class="w-full h-screen flex items-center justify-center" id="head">
-      <div class="md:grid md:grid-cols-2 content-center w-full">
-        <div
-          class="flex flex-col justify-center md:ml-24 text-center md:text-left"
-        >
-          <h1
-            :class="nightMode ? darkModeSet('font') : 'text-black'"
-            class="text-3xl md:text-left flex justify-center md:mr-6 font-bold font-mono md:text-6xl"
+  <div>
+    <div v-show="isPlaying">
+      <div :class="nightMode ? darkModeSet('bg') : ''" class="h-full">
+        <!-- night mode btn -->
+        <div class="fixed top-12 right-1 md:right-8">
+          <label
+            class="swap swap-rotate neutral justify-center mr-6 w-10 h-10 rounded-full shadow-lg"
+            :class="nightMode ? darkModeSet('block') : 'bg-slate-200'"
           >
-            Quiz ja pak quiz tueng quiz kat
-          </h1>
-          <p
-            class="md:text-left mt-8"
-            :class="nightMode ? darkModeSet('font') : 'text-black'"
-          >
-            A quiz IQ with this quiz
-          </p>
-
-          <div class="flex flex-row mt-5 md:justify-start justify-center">
-            <a href="#catagory">
-              <button
-                :class="nightMode ? darkModeSet('font') : 'text-black'"
-                class="bg-teal-500 p-2 pl-4 pr-4 mt-4 rounded-lg hover:bg-black hover:text-white shadow-lg"
-              >
-                get start
-              </button>
-            </a>
-            <button
-              :class="
-                nightMode ? darkModeSet('border') : 'border-black text-black'
-              "
-              class="bg-transparent border p-2 pl-4 pr-4 mt-4 rounded-lg hover:bg-black hover:text-white ml-3 shadow-lg"
-              @click="showHowtToPlay = !showHowtToPlay"
-            >
-              how to play
-            </button>
-          </div>
+            <input type="checkbox" v-model="nightMode" />
+            <moon class="swap-on w-5 h-5" />
+            <sun class="swap-off w-5 h-5" />
+          </label>
         </div>
 
-        <div class="w-11/12 hidden md:block">
-          <img src="./assets/image/cover.png" alt="quiz" class="" />
+        <!-- header -->
+        <div class="w-full h-screen flex items-center justify-center" id="head">
+          <div class="md:grid md:grid-cols-2 content-center w-full">
+            <div
+              class="flex flex-col justify-center md:ml-24 text-center md:text-left"
+            >
+              <h1
+                :class="nightMode ? darkModeSet('font') : 'text-black'"
+                class="text-3xl md:text-left flex justify-center md:mr-6 font-bold font-mono md:text-6xl"
+              >
+                Quiz ja pak quiz tueng quiz kat
+              </h1>
+              <p
+                class="md:text-left mt-8"
+                :class="nightMode ? darkModeSet('font') : 'text-black'"
+              >
+                A quiz IQ with this quiz
+              </p>
+
+              <div class="flex flex-row mt-5 md:justify-start justify-center">
+                <a href="#categories">
+                  <button
+                    :class="nightMode ? darkModeSet('font') : 'text-black'"
+                    class="bg-teal-500 p-2 pl-4 pr-4 mt-4 rounded-lg hover:bg-black hover:text-white shadow-lg"
+                  >
+                    get start
+                  </button>
+                </a>
+                <button
+                  :class="
+                    nightMode
+                      ? darkModeSet('border')
+                      : 'border-black text-black'
+                  "
+                  class="bg-transparent border p-2 pl-4 pr-4 mt-4 rounded-lg hover:bg-black hover:text-white ml-3 shadow-lg"
+                  @click="showHowtToPlay = !showHowtToPlay"
+                >
+                  how to play
+                </button>
+              </div>
+            </div>
+
+            <div class="w-11/12 hidden md:block">
+              <img src="./assets/image/cover.png" alt="quiz" class="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- CATEGORIES -->
+      <div id="categories" class="md:pt-3 py-7 px-10 text-center">
+        <div class="md:text-4xl text-2xl md:none font-extrabold md:my-12 my-6">
+          <h1 class="text-4xl font-extrabold">
+            What
+            <span
+              class="bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent"
+              >category</span
+            >
+            of quiz would you like to take?
+          </h1>
+        </div>
+        <div class="grid md:grid-cols-4 grid-cols-2 gap-5 md:px-48 font-medium">
+          <!-- for...in ; got property of object -->
+          <a
+            href="#level"
+            v-for="(prop, key) in categories"
+            :key="key"
+            class="py-5 p-3 text-center border rounded-lg shadow-lg hover:bg-gray-400 duration-200"
+            @click="key = prop[0]"
+            :class="
+              key === prop[0]
+                ? 'border-gray-200 bg-gray-200 shadow-black'
+                : 'bg-gray-200'
+            "
+          >
+            {{ key }}
+          </a>
+        </div>
+      </div>
+
+      <!-- LEVEL -->
+      <div id="level" class="md:py-6 py-10 px-9 text-center">
+        <div class="md:text-4xl text-2xl font-extrabold md:my-14 my-7">
+          <h1 class="text-4xl font-extrabold">
+            What
+            <span
+              class="bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent"
+              >difficulty</span
+            >
+            would you like to start with?
+          </h1>
+        </div>
+
+        <div
+          class="grid md:grid-cols-3 grid-cols-1 gap-7 md:px-32 px-5 font-medium"
+        >
+          <button
+            id="easy"
+            class="h-96 border rounded-3xl px-9 pt-64 text-start"
+            v-for="(level, key) in difficulties"
+            :key="key"
+            @click="difficulty = level.title"
+            :class="difficulty === level.title ? level.selectedbg : level.bg"
+          >
+            <p class="text-3xl font-extrabold">
+              {{ level.title.toLocaleUpperCase() }}
+            </p>
+            <p class="">{{ level.description }}</p>
+          </button>
+        </div>
+
+        <div
+          class="flex md:justify-between justify-end mt-10 md:px-32 md:none m-5"
+        >
+          <a
+            href="#test"
+            @click="
+              getQuestions(category, difficulty);
+              letsPlay = !letsPlay;
+              playAndPause();
+            "
+          >
+            <button
+              class="border rounded-lg py-1 px-5 bg-blue-500 text-white font-bold tracking-wide shadow-lg"
+              @click="
+                () => {
+                  getQuestions(category, difficulty);
+                  letsPlay = !letsPlay;
+                  playAndPause();
+                }
+              "
+            >
+              Lets Play
+            </button></a
+          >
         </div>
       </div>
     </div>
-  </div>
-  <!-- CATEGORIES -->
-  <div id="categories" class="md:pt-3 py-7 px-10 text-center">
-    <div class="md:text-4xl text-2xl md:none font-extrabold md:my-12 my-6">
-      What <span class="text-red-700">kind</span> of quiz would you like to take
-      ?
-    </div>
-    <div class="grid md:grid-cols-3 grid-cols-2 gap-5 md:px-48 font-medium">
-      <!-- for...in ; got property of object -->
-      <a
-        href="#level"
-        v-for="(prop, key) in categories"
-        :key="key"
-        class="p-8 text-center border rounded-lg shadow-lg"
-        @click="key = prop[0]"
-        :class="
-          key === prop[0]
-            ? 'border-stone-200 bg-stone-200 shadow-stone-500'
-            : 'bg-stone-200'
-        "
-      >
-        {{ key }}
-      </a>
-    </div>
-  </div>
-
-  <!-- LEVEL -->
-  <div id="level" class="md:py-6 py-10 px-9 text-center">
-    <div class="md:text-4xl text-2xl md:none font-extrabold md:my-14 my-7">
-      What <span class="text-red-700">difficulty</span> would you like to start
-      with ?
-    </div>
-
-    <div
-      class="grid md:grid-cols-3 grid-cols-1 gap-7 md:px-32 px-5 font-medium"
-    >
-      <button
-        id="easy"
-        class="h-96 border rounded-3xl px-9 pt-64 text-start"
-        v-for="(level, key) in difficulties"
-        :key="key"
-        @click="difficulty = level.title"
-        :class="difficulty === level.title ? level.selectedbg : level.bg"
-      >
-        <p class="text-3xl font-extrabold">
-          {{ level.title.toLocaleUpperCase() }}
-        </p>
-        <p class="">{{ level.description }}</p>
-      </button>
-    </div>
-
-    <div class="flex md:justify-between justify-end mt-10 md:px-32 md:none m-5">
-      <a href="#categories" class="md:block hidden"
-        ><button
-          class="border border-black rounded-lg py-1 px-5 font-semibold tracking-wide shadow-lg"
-        >
-          Back
-        </button></a
-      >
-      <a
-        href="#test"
-        @click="
-          getQuestions(category, difficulty);
-          letsPlay = !letsPlay;
-          playAndPause();
-        "
-      >
-        <button
-          class="border rounded-lg py-1 px-5 bg-blue-500 text-white font-bold tracking-wide shadow-lg"
-        >
-          Lets Play
-        </button></a
-      >
-    </div>
+    <div v-show="!isPlaying"></div>
   </div>
 </template>
 
