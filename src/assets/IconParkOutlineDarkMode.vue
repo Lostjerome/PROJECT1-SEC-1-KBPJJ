4b8c602a2b5eb62c87288677419f9dba868bf938
CRYINGV <60339342+CRYINGV@users.noreply.github.com>
2023-02-18 18:57:48 +0700

    Add files via upload

---
+++ src/assets/IconParkOutlineDarkMode.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="4"><path d="m24.003 4l5.27 5.27h9.457v9.456l5.27 5.27l-5.27 5.278v9.456h-9.456L24.004 44l-5.278-5.27H9.27v-9.456L4 23.997l5.27-5.27V9.27h9.456L24.003 4Z"></path><path d="M27 17c0 8-5 9-10 9c0 4 6.5 8 12 4s2-13-2-13Z"></path></g></svg>
+</template>
+
+<script>
+export default {
+  name: 'sun'
+}
+</script>
---
+++ src/assets/LineMdLightDark.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><defs><mask id="lineMdLightDark0"><circle cx="7.5" cy="7.5" r="5.5" fill="#fff"></circle><circle cx="7.5" cy="7.5" r="5.5"><animate fill="freeze" attributeName="cx" dur="0.4s" values="7.5;11"></animate><animate fill="freeze" attributeName="r" dur="0.4s" values="5.5;6.5"></animate></circle></mask><mask id="lineMdLightDark1"><g fill="#fff"><circle cx="12" cy="9" r="5.5"><animate fill="freeze" attributeName="cy" begin="1s" dur="0.5s" values="9;15"></animate></circle><g fillOpacity="0"><use href="#lineMdLightDark2" transform="rotate(-75 12 15)"></use><use href="#lineMdLightDark2" transform="rotate(-25 12 15)"></use><use href="#lineMdLightDark2" transform="rotate(25 12 15)"></use><use href="#lineMdLightDark2" transform="rotate(75 12 15)"></use><set attributeName="fill-opacity" begin="1.5s" to="1"></set></g></g><path d="M0 10h26v5h-26z"></path><path fill="none" stroke="#fff" strokeDasharray="26" strokeDashoffset="26" strokeLinecap="round" strokeWidth="2" d="M1 12h22"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.5s" dur="0.4s" values="26;52"></animate></path></mask><symbol id="lineMdLightDark2"><path d="M0 0"><animate fill="freeze" attributeName="d" begin="1.5s" dur="0.4s" values="M11 18h2L12 20z;M10.5 21.5h3L12 24z"></animate></path></symbol></defs><g fill="currentColor"><rect width="13" height="13" x="1" y="1" mask="url(#lineMdLightDark0)"></rect><path d="M-2 11h28v13h-28z" mask="url(#lineMdLightDark1)" transform="rotate(-45 12 12)"></path></g></svg>
+</template>
+
+<script>
+export default {
+  name: 'LineMdLightDark'
+}
+</script>
