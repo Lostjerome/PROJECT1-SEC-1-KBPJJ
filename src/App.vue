8066a2e42385f5ec4d3897bce3df6350391e3a1c
jerome <naronkrach@gmail.com>
2023-02-19 20:45:07 +0700

    :art: Improved structure to work properly

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,19 @@ const showHowtToPlay = ref(false);
 const questions = ref([]);
 const categories = ref();
 const score = ref(0);
-const currentQuestion = ref(0);
+const currentQuestionNumber = ref(0);
 const inputMusic = ref(null);
 
 const nightMode = ref(false);
 const isMusicPlay = ref(false);
+const categoriesSection = ref(null);
+const difficultiesSection = ref(null);
 
 const selectedCategory = ref("");
 const selectedDifficulty = ref("");
 
 const isPlaying = ref(false);
+const isFinish = ref(false);
 
 const difficulties = [
   {
@@ -40,29 +43,40 @@ const difficulties = [
   },
 ];
 
-fetch("https://the-trivia-api.com/api/categories")
-  .then((response) => response.json())
-  .then((data) => (categories.value = data));
+const getCategories = async () => {
+  const response = await fetch(
+    "https://the-trivia-api.com/api/categories?region=TH"
+  );
+  const jsonData = await response.json();
+  categories.value = jsonData;
+};
 
-function getQuestions(category, difficulty) {
-  fetch(
+const getQuestions = async (category, difficulty) => {
+  const response = await fetch(
     `https://the-trivia-api.com/api/questions?categories=${category}&limit=10&region=TH&difficulty=${difficulty}`
-  )
-    .then((response) => response.json())
-    .then((data) => (questions.value = data))
-    .catch((error) => console.log(error));
+  );
+  const jsonData = await response.json();
+  questions.value = jsonData;
+};
 
-  console.log(questions.value);
-  return questions.value;
-}
-getQuestions("film", "easy");
+// function getChoice(currentQuestion) {
+//   return shuffle([
+//     currentQuestion?.correctAnswer,
+//     ...currentQuestion?.incorrectAnswers,
+//   ]);
+// }
 
-function getChoice(choiceTarget) {
-  return shuffle([
-    choiceTarget?.correctAnswer,
-    ...choiceTarget?.incorrectAnswers,
-  ]);
-}
+const choices = computed(() => {
+  let choices = [];
+  if (questions.value[currentQuestionNumber.value]) {
+    choices = [
+      ...questions.value[currentQuestionNumber.value].incorrectAnswers,
+      questions.value[currentQuestionNumber.value].correctAnswer,
+    ];
+    shuffle(choices);
+  }
+  return choices;
+});
 
 function shuffle(array) {
   return array?.sort(() => Math.random() - 0.5);
@@ -89,6 +103,7 @@ const darkModeSet = (item) => {
     }
   }
 };
+getCategories();
 </script>
 
 <template>
@@ -127,21 +142,17 @@ const darkModeSet = (item) => {
               </p>
 
               <div class="flex flex-row mt-5 md:justify-start justify-center">
-                <a href="#categories">
-                  <button
-                    :class="nightMode ? darkModeSet('font') : 'text-black'"
-                    class="bg-teal-500 p-2 pl-4 pr-4 mt-4 rounded-lg hover:bg-black hover:text-white shadow-lg"
-                    @click="
-                      () => {
-                        selectedCategory = categories[i].reduce((a, b) =>
-                          a.length > b.length ? a : b
-                        );
-                      }
-                    "
-                  >
-                    get start
-                  </button>
-                </a>
+                <button
+                  @click="
+                    () => {
+                      categoriesSection.scrollIntoView({ behavior: 'smooth' });
+                    }
+                  "
+                  :class="nightMode ? darkModeSet('font') : 'text-black'"
+                  class="bg-teal-500 p-2 pl-4 pr-4 mt-4 rounded-lg hover:bg-black hover:text-white shadow-lg"
+                >
+                  Get started
+                </button>
                 <button
                   :class="
                     nightMode
@@ -151,7 +162,7 @@ const darkModeSet = (item) => {
                   class="bg-transparent border p-2 pl-4 pr-4 mt-4 rounded-lg hover:bg-black hover:text-white ml-3 shadow-lg"
                   @click="showHowtToPlay = !showHowtToPlay"
                 >
-                  how to play
+                  How to play
                 </button>
               </div>
             </div>
@@ -163,86 +174,95 @@ const darkModeSet = (item) => {
         </div>
       </div>
       <!-- CATEGORIES -->
-      <div id="categories" class="md:pt-3 py-7 px-10 text-center">
-        <div class="md:text-4xl text-2xl md:none font-extrabold md:my-12 my-6">
-          <h1 class="text-4xl font-extrabold">
-            What
-            <span
-              class="bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent"
-              >category</span
-            >
-            of quiz would you like to take?
-          </h1>
-        </div>
-        <div class="grid md:grid-cols-4 grid-cols-2 gap-5 md:px-48 font-medium">
-          <!-- for...in ; got property of object -->
-          <a
-            href="#level"
-            v-for="(prop, key) in categories"
-            :key="key"
-            class="py-5 p-3 text-center border rounded-lg shadow-lg hover:bg-gray-400 duration-200"
-            @click="key = prop[0]"
-            :class="
-              key === prop[0]
-                ? 'border-gray-200 bg-gray-200 shadow-black'
-                : 'bg-gray-200'
-            "
+      <div class="md:w-[52rem] m-auto">
+        <div ref="categoriesSection" class="md:pt-3 py-7 text-center">
+          <div class="md:text-4xl text-2xl font-extrabold md:my-12 my-6">
+            <h1 class="text-4xl font-extrabold">
+              What
+              <span
+                class="bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent"
+                >category</span
+              >
+              of quiz would you like to take?
+            </h1>
+          </div>
+          <div
+            class="grid md:grid-cols-4 grid-cols-2 gap-3 md:w-[52rem] m-auto font-medium"
           >
-            {{ key }}
-          </a>
-        </div>
-      </div>
-
-      <!-- LEVEL -->
-      <div id="level" class="md:py-6 py-10 px-9 text-center">
-        <div class="md:text-4xl text-2xl font-extrabold md:my-14 my-7">
-          <h1 class="text-4xl font-extrabold">
-            What
-            <span
-              class="bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent"
-              >difficulty</span
-            >
-            would you like to start with?
-          </h1>
-        </div>
-
-        <div
-          class="grid md:grid-cols-3 grid-cols-1 gap-7 md:px-32 px-5 font-medium"
-        >
-          <button
-            id="easy"
-            class="h-96 border rounded-3xl px-9 pt-64 text-start"
-            v-for="(level, key) in difficulties"
-            :key="key"
-            @click="selectedDifficulty = level.title"
-            :class="
-              selectedDifficulty === level.title ? level.selectedbg : level.bg
-            "
-          >
-            <p class="text-3xl font-extrabold">
-              {{ level.title.toLocaleUpperCase() }}
-            </p>
-            <p class="">{{ level.description }}</p>
-          </button>
-        </div>
-
-        <div
-          class="flex md:justify-between justify-end mt-10 md:px-32 md:none m-5"
-        >
-          <a href="#test">
+            <!-- for...in ; got property of object -->
             <button
-              class="border rounded-lg py-1 px-5 bg-blue-500 text-white font-bold tracking-wide shadow-lg"
+              v-for="(category, key) in [...Object.keys(categories)]"
+              :key="key"
+              :class="
+                categories[category].includes(selectedCategory)
+                  ? 'bg-gradient-to-r from-green-400 to-blue-500  text-white font-bold'
+                  : 'bg-white hover:bg-gray-100 text-black '
+              "
+              class="py-5 text-center border rounded-xl drop-shadow-md duration-200"
               @click="
                 () => {
-                  getQuestions(selectedCategory, selectedDifficulty);
-                  isPlaying = !isPlaying;
-                  playAndPause();
+                  selectedCategory = categories[category].reduce((a, b) =>
+                    a.length > b.length ? a : b
+                  );
+                  difficultiesSection.scrollIntoView({ behavior: 'smooth' });
                 }
               "
             >
-              Lets Play
-            </button></a
+              {{ category }}
+            </button>
+          </div>
+        </div>
+
+        <!-- DIFFICULTIES -->
+        <div ref="difficultiesSection" class="md:py-6 py-10 text-center">
+          <div class="md:text-4xl text-2xl font-extrabold md:my-14 my-7">
+            <h1 class="text-4xl font-extrabold">
+              What
+              <span
+                class="bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent"
+                >difficulty</span
+              >
+              would you like to start with?
+            </h1>
+          </div>
+
+          <div
+            class="grid md:grid-cols-3 grid-cols-1 gap-3 md:w-[52rem] m-auto"
           >
+            <button
+              v-for="(difficulty, key) in difficulties"
+              :key="key"
+              @click="selectedDifficulty = difficulty.title"
+              :class="
+                selectedDifficulty
+                  ? selectedDifficulty === difficulty.title
+                    ? difficulty.selectedbg
+                    : 'bg-gray-300'
+                  : difficulty.bg
+              "
+              class="border rounded-3xl px-5 pt-52 pb-10 text-start duration-200"
+            >
+              <p class="text-3xl font-extrabold">
+                {{ difficulty.title.toLocaleUpperCase() }}
+              </p>
+              <p class="">{{ difficulty.description }}</p>
+            </button>
+          </div>
+
+          <button
+            v-show="selectedCategory && selectedDifficulty"
+            class="mt-10 rounded-lg py-1 px-5 bg-blue-500 text-white font-bold tracking-wide shadow-lg"
+            @click="
+              () => {
+                getQuestions(selectedCategory, selectedDifficulty).then(() => {
+                  isPlaying = !isPlaying;
+                  playAndPause();
+                });
+              }
+            "
+          >
+            Lets Play
+          </button>
         </div>
       </div>
     </div>
@@ -252,20 +272,25 @@ const darkModeSet = (item) => {
       >
         <!-- nav-Bar -->
         <div
-          class="flex flex-row bg-slate-200 w-5/6 h-10 mt-6 items-center justify-between rounded-full"
+          class="p-2 flex flex-row bg-slate-200 w-5/6 mt-6 items-center justify-between rounded-full"
         >
           <!-- button-back -->
           <button
             class="border-solid border h-8 w-16 bg-white rounded-full ml-2 hover:bg-slate-400"
-            @click="isPlaying = !isPlaying"
+            @click="
+              () => {
+                isPlaying = !isPlaying;
+                playAndPause();
+                selectedCategory = '';
+                selectedDifficulty = '';
+              }
+            "
           >
             &#60;&nbsp;Back
           </button>
 
           <!-- score -->
-          <p
-            class="border-solid border h-8 w-24 mr-8 text-center bg-green-400 rounded-full"
-          >
+          <p class="p-2 px-6 text-center bg-green-400 rounded-full">
             Score:<span>&nbsp;{{ score }}</span>
           </p>
 
@@ -276,126 +301,125 @@ const darkModeSet = (item) => {
             DM
           </button>
         </div>
-
-        <!-- content -->
-        <div class="grid grid-col-1 w-4/6">
-          <!-- Question -->
-          <div
-            class="flex flex-col bg-white w-full h-auto mt-10 items-center justify-center rounded-3xl py-7 px-3"
-          >
-            <div class="text-center">
-              <p class="mt-3 text-gray-800">
-                Question {{ currentQuestion + 1 }}/10
-              </p>
-              <h2 class="font-semibold break-all m-3 text-4xl items-center">
-                {{ questions[currentQuestion]?.question }}
-              </h2>
-            </div>
-          </div>
-
-          <!-- Choice -->
-          <div
-            class="grid grid-cols-1 gap-3 bg-slate-300 mt-5 py-5 px-10 rounded-3xl w-full lg:grid-cols-2"
-          >
+        <div
+          class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        >
+          <!-- content -->
+          <div class="grid grid-col-1 w-[48em]">
+            <!-- Question -->
             <div
-              v-for="(choice, index) in getChoice(questions[currentQuestion])"
-              :key="index"
-              id="getC"
-              @click="
-                userClick = choice;
-                userClick === questions[currentQuestion]?.correctAnswer
-                  ? score++
-                  : (score += 0);
-                currentQuestion == questions.length - 1
-                  ? (currentQuestion += 0)
-                  : currentQuestion++;
-                countUserClick++;
-                countUserClick > questions.length - 1
-                  ? (checkFinishTest = true)
-                  : (checkFinishTest = false);
-              "
+              class="flex flex-col bg-white h-auto mt-10 items-center justify-center rounded-3xl py-5 px-2"
+            >
+              <div class="text-center">
+                <p class="mt-3 text-gray-800">
+                  Question {{ currentQuestionNumber + 1 }}/{{
+                    questions.length
+                  }}
+                </p>
+                <h2 class="font-semibold break-all m-3 text-2xl items-center">
+                  {{ questions[currentQuestionNumber]?.question }}
+                </h2>
+              </div>
+            </div>
+
+            <!-- Choice -->
+            <div
+              class="grid grid-cols-1 gap-3 bg-slate-300 mt-5 p-5 rounded-2xl w-full lg:grid-cols-2"
             >
               <button
-                class="bg-white rounded-3xl break-all py-3 px-2 w-full lg:px-10 hover:bg-slate-400"
+                v-for="(choice, key) in choices"
+                :key="key"
+                @click="
+                  choice === questions[currentQuestionNumber]?.correctAnswer &&
+                    score++;
+                  currentQuestionNumber == questions.length - 1
+                    ? (isFinish = true)
+                    : currentQuestionNumber++;
+                "
+                class="bg-white rounded-lg break-all py-3 w-full lg:px-10 hover:bg-slate-200 duration-200"
               >
                 {{ choice }}
               </button>
             </div>
+
+            <!-- reload-Button  -->
+            <div class="flex mt-7 justify-center">
+              <button
+                class="h-9 w-9 bg-white rounded-full mr-2 hover:bg-slate-400"
+                @click="
+                  score = 0;
+                  currentQuestionNumber = 0;
+                  isFinish = false;
+                  getQuestions(selectedCategory, selectedDifficulty);
+                "
+              >
+                RL
+              </button>
+            </div>
+          </div>
+        </div>
+        <!-- REF -->
+        <div
+          class="bg-gray-500 rounded-2xl w-[52rem] p-4 py-7 mt-10 text-center absolute left-1/2 -translate-x-1/2 bottom-5 text-white"
+        >
+          <p>
+            All questions are from
+            <a class="hover:text-orange-100" href="https://the-trivia-api.com"
+              >The Trivia API</a
+            >
+          </p>
+        </div>
+        <!-- result-score -->
+        <div
+          v-show="isFinish"
+          class="w-screen h-screen absolute bg-[#0000007a] bottom-0 backdrop-blur-sm"
+        ></div>
+        <div
+          class="flex flex-col bg-white text-center rounded-3xl p-8 items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          v-show="isFinish"
+        >
+          <p class="m-3 break-all">
+            You've finished all quizes. Here's your score
+          </p>
+
+          <!-- circle-score -->
+          <div class="bg-gray-300 rounded-full w-36 h-36 pt-9 mt-10">
+            <p class="text-4xl font-extrabold">{{ score }}</p>
+            <p class="font-medium">Out of {{ questions.length }}</p>
           </div>
 
-          <!-- reload-Button  -->
-          <div class="flex mt-7 justify-center">
+          <!-- button-action -->
+          <div
+            class="flex md:flex-row-reverse gap-2 font-bold mt-5 py-4 w-full text-center"
+          >
+            <!--button-play-again -->
             <button
-              class="h-9 w-9 bg-white rounded-full mr-2 hover:bg-slate-400"
+              class="bg-blue-500 text-white rounded-lg w-full py-2 hover:bg-blue-600 duration-200"
               @click="
                 score = 0;
-                currentQuestion = 0;
-                checkFinishTest = false;
-                countUserClick = 0;
+                currentQuestionNumber = 0;
+                isFinish = false;
                 getQuestions();
               "
             >
-              RL
+              Play again
             </button>
-          </div>
-
-          <!-- REF -->
-          <div
-            class="flex bg-gray-600 rounded-3xl w-full justify-center p-4 mt-10"
-          >
-            <p>
-              All questions are from
-              <a
-                class="hover:text-orange-100"
-                href="https://the-trivia-api.com/docs/#search"
-                >The Trivia API</a
-              >
-            </p>
-          </div>
-        </div>
-
-        <!-- result-score -->
-        <div
-          class="bg-fixed absolute min-w-full min-h-screen flex flex-col items-center"
-          v-show="checkFinishTest"
-        >
-          <div
-            class="flex flex-col w-3/6 h-full bg-white text-center mt-40 rounded-3xl p-2 items-center"
-          >
-            <p class="m-3 break-all">
-              You've finished all quizes. Here's your score
-            </p>
-
-            <!-- circle-score -->
-            <div class="bg-gray-300 rounded-full w-36 h-36 pt-9 mt-10">
-              <p class="text-4xl font-extrabold">{{ score }}</p>
-              <p class="font-medium">Out of 10</p>
-            </div>
-
-            <!-- button-action -->
-            <div
-              class="flex flex-col gap-4 font-bold mt-5 py-4 w-3/4 text-center"
+            <!--button-home -->
+            <button
+              @click="
+                () => {
+                  isPlaying = !isPlaying;
+                  isFinish = false;
+                  currentQuestionNumber = 0;
+                  playAndPause();
+                  selectedCategory = '';
+                  selectedDifficulty = '';
+                }
+              "
+              class="bg-gray-100 border border-black rounded-lg w-full p-2 py-2 hover:bg-gray-200 duration-200"
             >
-              <!--button-play-again -->
-              <button
-                class="bg-blue-500 text-white rounded-full w-full py-2 hover:bg-blue-600"
-                @click="
-                  score = 0;
-                  currentQuestion = 0;
-                  checkFinishTest = false;
-                  countUserClick = 0;
-                  getQuestions();
-                "
-              >
-                Play again
-              </button>
-              <!--button-home -->
-              <button
-                class="bg-gray-100 rounded-full p-2 py-2 hover:bg-gray-200"
-              >
-                Home
-              </button>
-            </div>
+              Home
+            </button>
           </div>
         </div>
       </div>
