5d4db13ab841e9e536b672233657c79e7af862c5
jerome <naronkrach@gmail.com>
2023-02-20 01:00:39 +0700

    :lipstick: Modified UI and :recycle: Refactor code

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,47 @@
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
+
 import moon from "./components/icons/moon.vue";
 import sun from "./components/icons/sun.vue";
+import reload from "./components/icons/reload.vue";
+import volumeOn from "./components/icons/volume-on.vue";
+import volumeOff from "./components/icons/volume-off.vue";
+import back from "./components/icons/arrow-left.vue";
 
-const showHowtToPlay = ref(false);
+import difficulties from "./utils/difficulties";
 
+// data
 const questions = ref([]);
-const categories = ref();
+const categories = ref([]);
+const selectedCategory = ref("");
+const selectedDifficulty = ref("");
 const score = ref(0);
 const currentQuestionNumber = ref(0);
 const inputMusic = ref(null);
 
+// status
 const nightMode = ref(false);
 const isMusicPlay = ref(false);
-const categoriesSection = ref(null);
-const difficultiesSection = ref(null);
-
-const selectedCategory = ref("");
-const selectedDifficulty = ref("");
-
 const isPlaying = ref(false);
 const isFinish = ref(false);
+const showHowToPlay = ref(false);
 
-const difficulties = [
-  {
-    title: "easy",
-    description: "A piece of cake.",
-    bg: "bg-green-500",
-    selectedbg: "bg-green-600 border-black",
-  },
-  {
-    title: "medium",
-    description: "Time to put your skill to the test!",
-    bg: "bg-amber-300",
-    selectedbg: "bg-amber-400 border-black",
-  },
-  {
-    title: "hard",
-    description: `Your're smart? Prove it with this one!`,
-    bg: "bg-red-500",
-    selectedbg: " border border-solid bg-red-600 border-black",
-  },
-];
+// ref
+const categoriesRef = ref(null);
+const difficultiesRef = ref(null);
+const playButtonRef = ref(null);
+
+const reloadPage = () => {
+  window.location.reload();
+};
+
+const restart = () => {
+  getQuestions(selectedCategory.value, selectedDifficulty.value).then(() => {
+    isFinish.value = false;
+    currentQuestionNumber.value = 0;
+    score.value = 0;
+  });
+};
 
 const getCategories = async () => {
   const response = await fetch(
@@ -78,9 +78,21 @@ const choices = computed(() => {
   return choices;
 });
 
-function shuffle(array) {
+const onSelectDifficulty = (difficulty) => {
+  selectedDifficulty.value = difficulty;
+};
+
+const ansOnClick = (ans) => {
+  ans === questions.value[currentQuestionNumber.value]?.correctAnswer &&
+    score.value++;
+  currentQuestionNumber.value == questions.value.length - 1
+    ? (isFinish.value = true)
+    : currentQuestionNumber.value++;
+};
+
+const shuffle = (array) => {
   return array?.sort(() => Math.random() - 0.5);
-}
+};
 
 const playAndPause = () => {
   isMusicPlay.value = !isMusicPlay.value;
@@ -103,6 +115,17 @@ const darkModeSet = (item) => {
     }
   }
 };
+
+watch(
+  () => selectedCategory.value && selectedDifficulty.value,
+  () => {
+    setTimeout(() => {
+      playButtonRef.value.scrollIntoView({ behavior: "smooth" });
+    }, 1);
+  }
+);
+
+// on setup
 getCategories();
 </script>
 
@@ -145,11 +168,11 @@ getCategories();
                 <button
                   @click="
                     () => {
-                      categoriesSection.scrollIntoView({ behavior: 'smooth' });
+                      categoriesRef.scrollIntoView({ behavior: 'smooth' });
                     }
                   "
                   :class="nightMode ? darkModeSet('font') : 'text-black'"
-                  class="bg-teal-500 p-2 pl-4 pr-4 mt-4 rounded-lg hover:bg-black hover:text-white shadow-lg"
+                  class="bg-teal-500 p-2 pl-4 pr-4 mt-4 rounded-lg hover:bg-black hover:text-white shadow-lg duration-100"
                 >
                   Get started
                 </button>
@@ -159,8 +182,8 @@ getCategories();
                       ? darkModeSet('border')
                       : 'border-black text-black'
                   "
-                  class="bg-transparent border p-2 pl-4 pr-4 mt-4 rounded-lg hover:bg-black hover:text-white ml-3 shadow-lg"
-                  @click="showHowtToPlay = !showHowtToPlay"
+                  class="bg-transparent border p-2 pl-4 pr-4 mt-4 rounded-lg hover:bg-black hover:text-white ml-3 shadow-lg duration-100"
+                  @click="showHowToPlay = !showHowToPlay"
                 >
                   How to play
                 </button>
@@ -173,10 +196,11 @@ getCategories();
           </div>
         </div>
       </div>
-      <!-- CATEGORIES -->
-      <div class="md:w-[52rem] m-auto">
-        <div ref="categoriesSection" class="md:pt-3 py-7 text-center">
-          <div class="md:text-4xl text-2xl font-extrabold md:my-12 my-6">
+
+      <div class="md:max-w-[52rem] m-auto px-5">
+        <!-- CATEGORIES -->
+        <div ref="categoriesRef" class="md:pt-3 py-7 text-center">
+          <div class="md:text-4xl text-2xl font-extrabold my-12">
             <h1 class="text-4xl font-extrabold">
               What
               <span
@@ -186,25 +210,23 @@ getCategories();
               of quiz would you like to take?
             </h1>
           </div>
-          <div
-            class="grid md:grid-cols-4 grid-cols-2 gap-3 md:w-[52rem] m-auto font-medium"
-          >
+          <div class="grid md:grid-cols-4 grid-cols-2 gap-3 m-auto font-medium">
             <!-- for...in ; got property of object -->
             <button
               v-for="(category, key) in [...Object.keys(categories)]"
               :key="key"
               :class="
                 categories[category].includes(selectedCategory)
-                  ? 'bg-gradient-to-r from-green-400 to-blue-500  text-white font-bold'
-                  : 'bg-white hover:bg-gray-100 text-black '
+                  ? 'bg-blue-500 text-white'
+                  : 'bg-white hover:bg-gray-100 text-black'
               "
-              class="py-5 text-center border rounded-xl drop-shadow-md duration-200"
+              class="py-5 px-3 text-center rounded-xl font-bold drop-shadow-lg duration-200 border border-black"
               @click="
                 () => {
                   selectedCategory = categories[category].reduce((a, b) =>
                     a.length > b.length ? a : b
                   );
-                  difficultiesSection.scrollIntoView({ behavior: 'smooth' });
+                  difficultiesRef.scrollIntoView({ behavior: 'smooth' });
                 }
               "
             >
@@ -214,7 +236,7 @@ getCategories();
         </div>
 
         <!-- DIFFICULTIES -->
-        <div ref="difficultiesSection" class="md:py-6 py-10 text-center">
+        <div ref="difficultiesRef" class="md:py-6 py-10 text-center">
           <div class="md:text-4xl text-2xl font-extrabold md:my-14 my-7">
             <h1 class="text-4xl font-extrabold">
               What
@@ -226,32 +248,31 @@ getCategories();
             </h1>
           </div>
 
-          <div
-            class="grid md:grid-cols-3 grid-cols-1 gap-3 md:w-[52rem] m-auto"
-          >
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-3 m-auto">
             <button
               v-for="(difficulty, key) in difficulties"
               :key="key"
-              @click="selectedDifficulty = difficulty.title"
+              @click="onSelectDifficulty(difficulty.title)"
               :class="
                 selectedDifficulty
                   ? selectedDifficulty === difficulty.title
                     ? difficulty.selectedbg
-                    : 'bg-gray-300'
+                    : 'bg-gray-300 hover:bg-gray-400'
                   : difficulty.bg
               "
-              class="border rounded-3xl px-5 pt-52 pb-10 text-start duration-200"
+              class="rounded-3xl border border-black px-5 pt-20 md:pt-52 pb-10 text-start duration-200 drop-shadow-lg"
             >
               <p class="text-3xl font-extrabold">
                 {{ difficulty.title.toLocaleUpperCase() }}
               </p>
-              <p class="">{{ difficulty.description }}</p>
+              <p>{{ difficulty.description }}</p>
             </button>
           </div>
 
           <button
+            ref="playButtonRef"
             v-show="selectedCategory && selectedDifficulty"
-            class="mt-10 rounded-lg py-1 px-5 bg-blue-500 text-white font-bold tracking-wide shadow-lg"
+            class="mt-10 rounded-lg py-2 px-7 bg-teal-500 hover:bg-black duration-200 text-white font-bold tracking-wide shadow-lg"
             @click="
               () => {
                 getQuestions(selectedCategory, selectedDifficulty).then(() => {
@@ -261,7 +282,7 @@ getCategories();
               }
             "
           >
-            Lets Play
+            Let's Play
           </button>
         </div>
       </div>
@@ -272,21 +293,14 @@ getCategories();
       >
         <!-- nav-Bar -->
         <div
-          class="p-2 flex flex-row bg-slate-200 w-5/6 mt-6 items-center justify-between rounded-full"
+          class="p-2 flex flex-row border border-black drop-shadow-lg bg-slate-200 w-[calc(100%-2.5rem)] max-w-[64rem] mt-6 items-center justify-between rounded-full"
         >
           <!-- button-back -->
           <button
-            class="border-solid border h-8 w-16 bg-white rounded-full ml-2 hover:bg-slate-400"
-            @click="
-              () => {
-                isPlaying = !isPlaying;
-                playAndPause();
-                selectedCategory = '';
-                selectedDifficulty = '';
-              }
-            "
+            class="border-solid border flex border-black p-2 px-4 bg-white rounded-full hover:bg-slate-100 duration-100"
+            @click="reloadPage"
           >
-            &#60;&nbsp;Back
+            <back class="w-6 h-6" />Back
           </button>
 
           <!-- score -->
@@ -295,20 +309,28 @@ getCategories();
           </p>
 
           <!-- dark-mode -->
-          <button
-            class="border-solid border h-9 w-9 bg-white rounded-full mr-2"
-          >
-            DM
-          </button>
+          <div class="space-x-2 flex">
+            <button
+              class="p-2 border border-black bg-white hover:bg-slate-100 rounded-full duration-100"
+            >
+              <volumeOff class="w-6 h-6" />
+            </button>
+            <button
+              class="p-2 border border-black bg-white hover:bg-slate-100 rounded-full duration-100"
+            >
+              <sun class="w-6 h-6" />
+            </button>
+          </div>
         </div>
-        <div
-          class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-        >
-          <!-- content -->
-          <div class="grid grid-col-1 w-[48em]">
+
+        <!-- content -->
+        <div class="">
+          <div
+            class="w-full p-5 md:max-w-[48rem] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          >
             <!-- Question -->
             <div
-              class="flex flex-col bg-white h-auto mt-10 items-center justify-center rounded-3xl py-5 px-2"
+              class="flex flex-col bg-white border border-black h-auto mt-10 items-center justify-center rounded-3xl py-5 px-2"
             >
               <div class="text-center">
                 <p class="mt-3 text-gray-800">
@@ -316,7 +338,7 @@ getCategories();
                     questions.length
                   }}
                 </p>
-                <h2 class="font-semibold break-all m-3 text-2xl items-center">
+                <h2 class="font-semibold m-3 text-2xl items-center">
                   {{ questions[currentQuestionNumber]?.question }}
                 </h2>
               </div>
@@ -324,19 +346,13 @@ getCategories();
 
             <!-- Choice -->
             <div
-              class="grid grid-cols-1 gap-3 bg-slate-300 mt-5 p-5 rounded-2xl w-full lg:grid-cols-2"
+              class="grid grid-cols-1 gap-3 border border-black bg-slate-300 mt-5 p-5 rounded-3xl md:grid-cols-2"
             >
               <button
                 v-for="(choice, key) in choices"
                 :key="key"
-                @click="
-                  choice === questions[currentQuestionNumber]?.correctAnswer &&
-                    score++;
-                  currentQuestionNumber == questions.length - 1
-                    ? (isFinish = true)
-                    : currentQuestionNumber++;
-                "
-                class="bg-white rounded-lg break-all py-3 w-full lg:px-10 hover:bg-slate-200 duration-200"
+                @click="ansOnClick(choice)"
+                class="bg-white rounded-xl border border-black py-4 px-2 w-full hover:bg-slate-100 duration-100"
               >
                 {{ choice }}
               </button>
@@ -345,22 +361,17 @@ getCategories();
             <!-- reload-Button  -->
             <div class="flex mt-7 justify-center">
               <button
-                class="h-9 w-9 bg-white rounded-full mr-2 hover:bg-slate-400"
-                @click="
-                  score = 0;
-                  currentQuestionNumber = 0;
-                  isFinish = false;
-                  getQuestions(selectedCategory, selectedDifficulty);
-                "
+                class="p-2 bg-white border border-black rounded-full mr-2 hover:bg-slate-100 duration-100"
+                @click="restart"
               >
-                RL
+                <reload class="w-8 h-8" />
               </button>
             </div>
           </div>
         </div>
         <!-- REF -->
         <div
-          class="bg-gray-500 rounded-2xl w-[52rem] p-4 py-7 mt-10 text-center absolute left-1/2 -translate-x-1/2 bottom-5 text-white"
+          class="bg-gray-500 border border-black rounded-2xl w-[calc(100%-2.5rem)] max-w-[52rem] p-4 py-7 mt-10 text-center absolute left-1/2 -translate-x-1/2 bottom-5 text-white"
         >
           <p>
             All questions are from
@@ -375,15 +386,20 @@ getCategories();
           class="w-screen h-screen absolute bg-[#0000007a] bottom-0 backdrop-blur-sm"
         ></div>
         <div
-          class="flex flex-col bg-white text-center rounded-3xl p-8 items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          class="flex flex-col border border-black bg-white text-center rounded-3xl p-8 py-5 items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
           v-show="isFinish"
         >
+          <p class="font-bold text-2xl">Hooray!</p>
           <p class="m-3 break-all">
-            You've finished all quizes. Here's your score
+            You've finished all {{ selectedCategory }} quizes with
+            {{ selectedDifficulty }} level. <br />
+            Here's your score
           </p>
 
           <!-- circle-score -->
-          <div class="bg-gray-300 rounded-full w-36 h-36 pt-9 mt-10">
+          <div
+            class="bg-gray-300 border border-black rounded-full w-36 h-36 pt-9 mt-10"
+          >
             <p class="text-4xl font-extrabold">{{ score }}</p>
             <p class="font-medium">Out of {{ questions.length }}</p>
           </div>
@@ -394,31 +410,17 @@ getCategories();
           >
             <!--button-play-again -->
             <button
-              class="bg-blue-500 text-white rounded-lg w-full py-2 hover:bg-blue-600 duration-200"
-              @click="
-                score = 0;
-                currentQuestionNumber = 0;
-                isFinish = false;
-                getQuestions();
-              "
+              class="bg-teal-500 text-white rounded-lg w-full py-2 hover:bg-teal-600 duration-200"
+              @click="restart"
             >
               Play again
             </button>
             <!--button-home -->
             <button
-              @click="
-                () => {
-                  isPlaying = !isPlaying;
-                  isFinish = false;
-                  currentQuestionNumber = 0;
-                  playAndPause();
-                  selectedCategory = '';
-                  selectedDifficulty = '';
-                }
-              "
+              @click="reloadPage"
               class="bg-gray-100 border border-black rounded-lg w-full p-2 py-2 hover:bg-gray-200 duration-200"
             >
-              Home
+              Try other one
             </button>
           </div>
         </div>
---
+++ src/utils/difficulties.js
@@ -0,0 +1,25 @@
+const difficulties = [
+  {
+    title: "easy",
+    description: "A piece of cake.",
+    bg: "bg-green-500 hover:bg-green-600",
+    selectedbg:
+      "bg-gradient-to-r md:bg-gradient-to-b from-teal-400 to-green-600",
+  },
+  {
+    title: "medium",
+    description: "Time to put your skill to the test!",
+    bg: "bg-amber-300 hover:bg-amber-400",
+    selectedbg:
+      "bg-gradient-to-r md:bg-gradient-to-b from-yellow-300 to-amber-500 ",
+  },
+  {
+    title: "hard",
+    description: `Your're smart? Prove it with this one!`,
+    bg: "bg-red-500 hover:bg-red-600",
+    selectedbg:
+      "bg-gradient-to-r md:bg-gradient-to-b from-red-400 to-rose-600 ",
+  },
+];
+
+export default difficulties;
