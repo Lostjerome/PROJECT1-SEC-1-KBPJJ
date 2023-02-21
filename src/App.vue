3e82faeb8f3b7e979b1082401620c5101c895d59
jerome <naronkrach@gmail.com>
2023-02-21 23:04:47 +0700

    :recycle: Refactor code and :sparkles: Add reveal answer feature

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import back from "./components/icons/arrow-left.vue";
 
 import themeSong from "./assets/audio/gameShow.mp3";
 
-import difficulties from "./utils/difficulties";
+import difficulties from "./constant/difficulties";
 
 // data
 const questions = ref([]);
@@ -23,15 +23,15 @@ const inputMusicRef = ref(null);
 
 // theme
 const themeDark = ref({
-  "background-color": "#222222",
+  "background-color": "#1F2128",
   color: "#FFFFFF",
 });
 const themeLight = ref({
   "background-color": "#f2f2f2",
   color: "#000000",
 });
-const buttonDark = ref("bg-neutral-500 hover:bg-neutral-600 border-white ");
-const buttonLight = ref("bg-white hover:bg-slate-100 border-black ");
+const buttonDark = ref("bg-[#4a5066] md:hover:bg-[#3d4253] border-white ");
+const buttonLight = ref("bg-white md:hover:bg-slate-100 border-black ");
 
 // status
 const darkMode = ref(false);
@@ -39,6 +39,7 @@ const isMusicPlaying = ref(false);
 const isPlaying = ref(false);
 const isFinish = ref(false);
 const showHowToPlay = ref(false);
+const showAnswer = ref(false);
 
 // ref
 const categoriesRef = ref(null);
@@ -56,12 +57,11 @@ const backToHome = () => {
   }, 1);
 };
 
-const restart = () => {
+const restartPlaying = () => {
   getQuestions(selectedCategory.value, selectedDifficulty.value).then(() => {
     isFinish.value = false;
     currentQuestionNumber.value = 0;
     score.value = 0;
-    playAndPause("restart");
   });
 };
 
@@ -105,11 +105,16 @@ const toggleDarkMode = () => {
 };
 
 const ansOnClick = (ans) => {
+  showAnswer.value = true;
   ans === questions.value[currentQuestionNumber.value]?.correctAnswer &&
     score.value++;
-  currentQuestionNumber.value == questions.value.length - 1
-    ? (isFinish.value = true)
-    : currentQuestionNumber.value++;
+
+  setTimeout(() => {
+    currentQuestionNumber.value == questions.value.length - 1
+      ? (isFinish.value = true)
+      : currentQuestionNumber.value++;
+    showAnswer.value = false;
+  }, 1000);
 };
 
 const startPlaying = () => {
@@ -127,6 +132,8 @@ const shuffle = (array) => {
 };
 
 const playAndPause = (param) => {
+  inputMusicRef.value.volume = 0.05;
+
   if (param === "stop") {
     isMusicPlaying.value = false;
     inputMusicRef.value.pause();
@@ -140,8 +147,10 @@ const playAndPause = (param) => {
     return;
   }
   isMusicPlaying.value = !isMusicPlaying.value;
-  if (isMusicPlaying.value) inputMusicRef.value.play();
-  else inputMusicRef.value.pause();
+  if (isMusicPlaying.value) {
+    inputMusicRef.value.play();
+    //decrease volume
+  } else inputMusicRef.value.pause();
 };
 
 watch(
@@ -172,7 +181,7 @@ getCategories();
         </button>
 
         <!-- header -->
-        <div class="w-full h-screen flex items-center justify-center" id="head">
+        <div class="w-full h-screen flex items-center justify-center">
           <div class="md:grid md:grid-cols-2 content-center w-full">
             <div
               class="flex flex-col justify-center md:ml-24 text-center md:text-left"
@@ -194,13 +203,13 @@ getCategories();
                     }
                   "
                   :class="darkMode ? 'border-white' : 'border-black'"
-                  class="bg-teal-500 text-white font-bold p-2 pl-4 pr-4 mt-4 rounded-lg border hover:bg-black hover:text-white shadow-lg duration-100"
+                  class="bg-teal-500 text-white font-bold p-2 pl-4 pr-4 mt-4 rounded-lg border md:hover:bg-black md:hover:text-white shadow-lg duration-200"
                 >
                   Get started
                 </button>
                 <button
-                  :class="darkMode ? 'border-white' : 'border-black'"
-                  class="border p-2 pl-4 pr-4 mt-4 rounded-lg hover:bg-black hover:text-white ml-3 shadow-lg duration-100"
+                  :class="darkMode ? 'border-white' : 'border-black '"
+                  class="border p-2 pl-4 pr-4 mt-4 rounded-lg md:hover:bg-black md:hover:text-white ml-3 shadow-lg duration-200"
                   @click="showHowToPlay = !showHowToPlay"
                 >
                   How to play
@@ -214,7 +223,7 @@ getCategories();
           </div>
         </div>
 
-        <div class="md:max-w-[52rem] m-auto px-5">
+        <div class="md:max-w-[60rem] m-auto px-5">
           <!-- CATEGORIES -->
           <div ref="categoriesRef" class="md:pt-3 py-7 text-center">
             <div class="md:text-4xl text-2xl font-extrabold my-12">
@@ -284,7 +293,7 @@ getCategories();
                       ? difficulty.selectedbg
                       : darkMode
                       ? buttonDark
-                      : 'bg-gray-300 hover:bg-gray-400 border-black'
+                      : 'bg-gray-300 md:hover:bg-gray-400 border-black'
                     : difficulty.bg
                 "
                 class="rounded-3xl border-black border px-5 pt-20 md:pt-52 pb-10 text-start duration-200 drop-shadow-lg"
@@ -295,28 +304,31 @@ getCategories();
                 <p>{{ difficulty.description }}</p>
               </button>
             </div>
-
-            <button
-              ref="playButtonRef"
-              v-show="selectedCategory && selectedDifficulty"
-              class="mt-10 rounded-lg py-2 px-7 bg-teal-500 hover:bg-black duration-200 text-white font-bold tracking-wide shadow-lg"
-              @click="startPlaying"
-            >
-              Let's Play
-            </button>
+            <div class="flex justify-between">
+              <div></div>
+              <button
+                ref="playButtonRef"
+                v-show="selectedCategory && selectedDifficulty"
+                :class="darkMode ? 'border-white' : 'border-black'"
+                class="mt-10 rounded-lg py-2 px-7 bg-teal-500 md:hover:bg-black duration-200 text-white font-bold tracking-wide shadow-lg border"
+                @click="startPlaying"
+              >
+                Let's Play
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
     <div :style="darkMode ? themeDark : ''" v-show="isPlaying">
       <div
-        :class="darkMode ? 'bg-neutral-800' : 'bg-gray-400'"
+        :class="darkMode ? 'bg-[#1F2128]' : 'bg-gray-400'"
         class="min-h-screen min-w-full flex flex-col items-center"
       >
         <!-- nav-Bar -->
         <div
           :class="
-            darkMode ? 'bg-neutral-700 border-white' : 'bg-white border-black'
+            darkMode ? 'bg-[#3d4253] border-white' : 'bg-white border-black'
           "
           class="p-1 md:p-2 flex flex-row fixed z-10 border drop-shadow-lg w-[calc(100%-2.5rem)] max-w-[64rem] mt-6 items-center justify-between rounded-full"
         >
@@ -369,7 +381,7 @@ getCategories();
           <!-- Question -->
           <div
             :class="
-              darkMode ? 'bg-neutral-500 border-white' : 'bg-white border-black'
+              darkMode ? 'bg-[#4a5066] border-white' : 'bg-white border-black'
             "
             class="flex flex-col border h-auto mt-10 items-center justify-center rounded-2xl md:rounded-3xl py-3 md:py-5 md:px-2"
           >
@@ -390,7 +402,7 @@ getCategories();
           <div
             :class="
               darkMode
-                ? 'bg-neutral-600 border-white'
+                ? 'bg-[#3d4253] border-white'
                 : 'bg-slate-300 border-black '
             "
             class="grid grid-cols-1 gap-3 border mt-5 p-3 md:p-5 rounded-2xl md:rounded-3xl md:grid-cols-2"
@@ -398,8 +410,18 @@ getCategories();
             <button
               v-for="(choice, key) in choices"
               :key="key"
-              @click="ansOnClick(choice)"
-              :class="darkMode ? buttonDark : buttonLight"
+              @click="!showAnswer && ansOnClick(choice)"
+              :class="
+                showAnswer
+                  ? questions[currentQuestionNumber]?.correctAnswer === choice
+                    ? 'bg-green-400 border-green-700 text-black'
+                    : darkMode
+                    ? buttonDark
+                    : buttonLight
+                  : darkMode
+                  ? buttonDark
+                  : buttonLight
+              "
               class="rounded-lg md:rounded-xl border py-3 md:py-4 px-2 w-full duration-100"
             >
               {{ choice }}
@@ -411,11 +433,11 @@ getCategories();
             <button
               :class="
                 darkMode
-                  ? 'bg-neutral-500 border-white hover:bg-neutral-600 fill-white'
-                  : 'bg-white border-black hover:bg-slate-100'
+                  ? buttonDark + 'fill-white'
+                  : 'bg-white border-black md:hover:bg-slate-100'
               "
               class="p-2 border rounded-full mr-2 duration-100"
-              @click="restart"
+              @click="restartPlaying"
             >
               <reload class="w-6 h-6 md:w-8 md:h-8" />
             </button>
@@ -423,16 +445,17 @@ getCategories();
         </div>
         <!-- REF -->
         <div
-          :class="darkMode ? 'bg-neutral-700' : 'bg-slate-300 border-black'"
+          :class="darkMode ? 'bg-[#3d4253]' : 'bg-slate-300 border-black'"
           class="border rounded-lg md:rounded-2xl w-[calc(100%-2.5rem)] max-w-[52rem] p-2 py-4 md:p-4 md:py-7 mt-10 text-center fixed left-1/2 -translate-x-1/2 bottom-5"
         >
           <p>
             All questions are from
             <a
+              target="_blank"
               :class="
                 darkMode
-                  ? 'text-purple-300 hover:text-purple-400'
-                  : 'text-purple-600 hover:text-purple-700'
+                  ? 'text-purple-300 md:hover:text-purple-400'
+                  : 'text-purple-600 md:hover:text-purple-700'
               "
               class="font-bold"
               href="https://the-trivia-api.com"
@@ -443,11 +466,11 @@ getCategories();
         <!-- result-score -->
         <div
           v-show="isFinish"
-          class="w-screen h-screen absolute bg-[#0000007a] bottom-0 backdrop-blur-sm"
+          class="w-screen h-full absolute bg-[#0000007a] bottom-0 backdrop-blur-sm z-20"
         ></div>
         <div
-          :class="darkMode ? 'bg-neutral-700' : 'bg-white border-black'"
-          class="flex flex-col w-[calc(100%-2.5rem)] max-w-[32em] border text-center rounded-3xl p-8 py-5 items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          :class="darkMode ? 'bg-[#4a5066]' : 'bg-white border-black'"
+          class="flex flex-col z-20 w-[calc(100%-2.5rem)] max-w-[32em] border text-center rounded-3xl p-8 py-5 items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
           v-show="isFinish"
         >
           <p class="font-bold text-2xl">Hooray!</p>
@@ -459,7 +482,7 @@ getCategories();
 
           <!-- circle-score -->
           <div
-            :class="darkMode ? 'bg-neutral-500' : 'bg-gray-300 border-black'"
+            :class="darkMode ? 'bg-[#3d4253]' : 'bg-gray-300 border-black'"
             class="border rounded-full w-36 h-36 pt-9 mt-10"
           >
             <p class="text-4xl font-extrabold">{{ score }}</p>
@@ -472,8 +495,8 @@ getCategories();
           >
             <!--button-play-again -->
             <button
-              class="bg-teal-500 text-white rounded-lg w-full py-2 hover:bg-teal-600 duration-200"
-              @click="restart"
+              class="bg-teal-500 text-white rounded-lg w-full py-2 md:hover:bg-teal-600 duration-200"
+              @click="restartPlaying"
             >
               Play again
             </button>
